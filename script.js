c4b54e32729087c4fc06961b6fde3620031b78dd
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-04 20:59:46 -0300

    Pixels Art - Requisito 8 e 9

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const colorPallete = document.querySelectorAll('.color');
 const pixelBoard = document.querySelector('#pixel-board');
-const pixel = document.querySelectorAll('article.pixel');
+const pixel = document.getElementsByClassName('pixel');
 const colorBlack = document.querySelector('.color-black');
 const colorRed = document.querySelector('.color-red');
 const colorBlue = document.querySelector('.color-blue');
 const colorGreen = document.querySelector('.color-green');
+const clear = document.querySelector('#clear-board');
 
 let selectedColor = undefined;
 
@@ -23,6 +24,31 @@ function createPixels() {
   }
 }
 
+function paintPixels(selectedColor) {
+  if (
+    colorBlack.className.includes('selected') ||
+    colorRed.className.includes('selected') ||
+    colorBlue.className.includes('selected') ||
+    colorGreen.className.includes('selected')
+  ) {
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].addEventListener('click', () => {
+        pixel[i].style.backgroundColor = selectedColor;
+      });
+    }
+  }
+}
+
+function paintBlack() {
+  colorBlack.classList.add('selected');
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].addEventListener('click', () => {
+      selectedColor = getComputedStyle(colorPallete[0]).backgroundColor;
+      pixel[i].style.backgroundColor = selectedColor;
+    });
+  }
+}
+
 colorBlack.addEventListener('click', () => {
   if (
     colorBlue.className.includes('selected') ||
@@ -33,60 +59,64 @@ colorBlack.addEventListener('click', () => {
     colorRed.classList.remove('selected');
     colorGreen.classList.remove('selected');
   }
-    colorBlack.classList.add('selected');
+  colorBlack.classList.add('selected');
   selectedColor = getComputedStyle(colorPallete[0]).backgroundColor;
-  console.log(selectedColor);
+  paintPixels(selectedColor);
 });
 
-  colorRed.addEventListener('click', () => {
-    if (
-      colorBlack.className.includes('selected') ||
-      colorBlue.className.includes('selected') ||
-      colorGreen.className.includes('selected')
-    ) {
-      colorBlack.classList.remove('selected');
-      colorBlue.classList.remove('selected');
-      colorGreen.classList.remove('selected');
-    }
-      colorRed.classList.add('selected');
+colorRed.addEventListener('click', () => {
+  if (
+    colorBlack.className.includes('selected') ||
+    colorBlue.className.includes('selected') ||
+    colorGreen.className.includes('selected')
+  ) {
+    colorBlack.classList.remove('selected');
+    colorBlue.classList.remove('selected');
+    colorGreen.classList.remove('selected');
+  }
+  colorRed.classList.add('selected');
   selectedColor = getComputedStyle(colorPallete[1]).backgroundColor;
-  console.log(selectedColor);
-
+  paintPixels(selectedColor);
 });
 
 colorBlue.addEventListener('click', () => {
-    if (
-      colorBlack.className.includes('selected') ||
-      colorRed.className.includes('selected') ||
-      colorGreen.className.includes('selected')
-    ) {
-      colorBlack.classList.remove('selected');
-      colorRed.classList.remove('selected');
-      colorGreen.classList.remove('selected');
-    }
-      colorBlue.classList.add('selected');
+  if (
+    colorBlack.className.includes('selected') ||
+    colorRed.className.includes('selected') ||
+    colorGreen.className.includes('selected')
+  ) {
+    colorBlack.classList.remove('selected');
+    colorRed.classList.remove('selected');
+    colorGreen.classList.remove('selected');
+  }
+  colorBlue.classList.add('selected');
   selectedColor = getComputedStyle(colorPallete[2]).backgroundColor;
-  console.log(selectedColor);
-
+  paintPixels(selectedColor);
 });
 
 colorGreen.addEventListener('click', () => {
-    if (
-      colorBlack.className.includes('selected') ||
-      colorRed.className.includes('selected') ||
-      colorBlue.className.includes('selected')
-    ) {
-      colorBlack.classList.remove('selected');
-      colorRed.classList.remove('selected');
-      colorBlue.classList.remove('selected');
-    }
-      colorGreen.classList.add('selected');
+  if (
+    colorBlack.className.includes('selected') ||
+    colorRed.className.includes('selected') ||
+    colorBlue.className.includes('selected')
+  ) {
+    colorBlack.classList.remove('selected');
+    colorRed.classList.remove('selected');
+    colorBlue.classList.remove('selected');
+  }
+  colorGreen.classList.add('selected');
   selectedColor = getComputedStyle(colorPallete[3]).backgroundColor;
-  console.log(selectedColor);
+  paintPixels(selectedColor);
+});
 
+clear.addEventListener('click', () => {
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = 'white';
+  }
 });
 
 window.onload = function () {
   setPaletteColor();
   createPixels();
+  paintBlack();
 };
