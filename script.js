d7cc7e9f294165b212e5bacf6f6d861027b1d52f
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-04 16:20:05 -0300

    Pixels Art - Requisito 4

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const colorPallete = document.querySelectorAll('.color');
+const pixelBoard = document.querySelector('#pixel-board');
 
-window.onload = function() {
-    colorPallete[0].style.backgroundColor = 'black';
-    colorPallete[1].style.backgroundColor = 'red';
-    colorPallete[2].style.backgroundColor = 'blue';
-    colorPallete[3].style.backgroundColor = 'green';
+function setPaletteColor() {
+  colorPallete[0].style.backgroundColor = 'black';
+  colorPallete[1].style.backgroundColor = 'red';
+  colorPallete[2].style.backgroundColor = 'blue';
+  colorPallete[3].style.backgroundColor = 'green';
 }
+
+function createPixels() {
+  for (let index = 1; index <= 25; index += 1) {
+    const pixel = document.createElement('article');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
+}
+
+window.onload = function () {
+  setPaletteColor();
+  createPixels();
+};
