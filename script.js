32b0e9b41bed43e5889584ee8db5e82c462dc665
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-04 15:35:37 -0300

    Pixels Art - Requisito 2

---
+++ script.js
@@ -0,0 +1,8 @@
+const colorPallete = document.querySelectorAll('.color');
+
+window.onload = function() {
+    colorPallete[0].style.backgroundColor = 'black';
+    colorPallete[1].style.backgroundColor = 'red';
+    colorPallete[2].style.backgroundColor = 'blue';
+    colorPallete[3].style.backgroundColor = 'green';
+}
